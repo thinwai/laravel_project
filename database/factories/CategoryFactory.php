21243ea18354efb97cfeca84c6209c35ad09b031
thinwai <thinthinwai215@gmail.com>
2021-12-08 16:14:42 +0630

    Article_Project_Laravel_v1

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CategoryFactory extends Factory
+{
+    protected $model = \App\Models\Category::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            "name" => ucwords($this->faker->word)
+        ];
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\Article::factory(20)->create();
+        \App\Models\Category::factory(5)->create();
+        \App\Models\Comment::factory(40)->create();
+        //factory(App\Article::class, 20)->create();
+        \App\Models\User::factory()->create([
+            "name" => "Alice",
+            "email" => "alice@gmail.com",
+        ]);
+        \App\Models\User::factory()->create([
+            "name" => "Bob",
+            "email" => "bob@gmail.com",
+        ]);
+           
+    }
+}
